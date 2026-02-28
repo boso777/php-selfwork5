c9dfa445dc014da4609cd8823f44750e9aef5547
Boso <82522285+boso777@users.noreply.github.com>
2026-02-28 09:29:43 +0100

    selfwork php 5

--- index.php
+++ index.php
@@ -2,20 +2,23 @@
 
 // Dato un array di numeri a scelta, scrivere un programma che calcoli la media solo dei numeri pari contenuti all’interno dell’array.
 
-$numbers = [1, 45, 99, 3349, 33, 47, 99, 89, 90, 3];
+$numbers = [1, 45, 47, 99, 89, 90, 3, 54, 88, 22];
 
 $sum = 0;
+$pari = [];
 
 foreach($numbers as $number){
+
         if($number % 2 == 0){
+            $pari[] = $number;
             $sum = $sum + $number;
-        }
+        };
 };
 
-$average = $sum / count($numbers);
+$average = $sum / count($pari);
 
 
+echo "La media dei numeri presenti nell'array numbers è " . $average . "\n";
 
-echo "La media dei numeri presenti nell'array " . $average;
 
 ?>
